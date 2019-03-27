c4101c1c3f31f5f2cf41405c2341413603255446
Emre Güdür <emre.gudur@gmail.com>
2019-03-27 13:09:13 +0200

    anasayfa'dan otobüsün durak detayına kadar test ediliyor.

--- src/test/testIett.java
+++ src/test/testIett.java
@@ -6,15 +6,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class testIett {
 	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
 
-		//Now you can Initialize marionette driver to launch firefox
-		DesiredCapabilities capabilities = DesiredCapabilities.firefox();
-		capabilities.setCapability("marionette", true);
 		WebDriver driver = new FirefoxDriver();
 		driver.get("https://www.iett.istanbul/");
 		WebElement link;
@@ -23,5 +19,33 @@ public class testIett {
 		
 		Thread.sleep(1000);
 		driver.findElement(By.partialLinkText("Hat")).click();
+		
+		Thread.sleep(5000);
+		
+		//ÝNPUTUN YÜKLENMESÝ ÝÇÝN TIKLANMASI GEREKÝYOR
+		driver.findElement(By.id("select2-DetailSelect-container")).click();
+		Thread.sleep(2000);
+		//ÝNPUTA DEÐER GÖNDERÝLÝYOR
+		driver.findElement(By.className("select2-search__field")).sendKeys("500T");
+		
+		Thread.sleep(1000);
+		//AÇILIR MENÜ ELEMANINA TIKLANIYOR
+		driver.findElement(By.className("select2-results__option")).click();
+		
+		Thread.sleep(1000);
+		//AÇILAN SAYFADA DURAK DETAYLARI LÝNK AÇILIYOR
+		driver.findElement(By.xpath("/html/body/header[2]/div/div/nav/a[2]/span")).click();
+		
+		
+		Thread.sleep(1000);
+		//DURAK DETAYINA TIKLANIYOR
+		driver.findElement(By.cssSelector("div.DetailTables:nth-child(1) > div:nth-child(1) > ol:nth-child(2) > li:nth-child(19) > div:nth-child(3) > a:nth-child(2)")).click();
+		
+		Thread.sleep(2000);
+		driver.quit();
+	
+		
+		
+		
 	}
 }
